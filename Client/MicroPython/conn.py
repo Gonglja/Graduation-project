feb4aa7d7b83981de397191345578f8f432b6e57
wslibeia <wslibeia@gmail.com>
2019-05-11 20:08:25 +0800

    修改文件

--- Client/MicroPython/conn.py
+++ Client/MicroPython/conn.py
@@ -1,16 +1,19 @@
 import network
-SSID = 'Power Laboratory 1221'
-PASSWORD = 'Dldz1221!'
-def do_connect():
+SSID = ['Power Laboratory 1221','PandoraBox_010203','Gonglja']
+PASSWORD = ['Dldz1221!','1552375337a','12345678..']
+def do_connect(i):
     import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
         #sta_if.connect('<essid>', '<password>')
-        sta_if.connect(SSID, PASSWORD)
+        #for i in range(2):
+        sta_if.connect(SSID[i], PASSWORD[i])
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
-	
-do_connect()	
+
+#for i in range(0,3):	
+do_connect(1)
+    	
--- Client/MicroPython/main.py
+++ Client/MicroPython/main.py
@@ -4,9 +4,11 @@ from simple import MQTTClient
 
 c = MQTTClient("umqtt_client","139.199.95.172", 1883, "admin_esp", "0800")
 c.connect()
-def m(t,m):   
-    c.publish(t, m)
+def m(t,h,s):   
+    c.publish("T", t)
+    c.publish("H", h)
+    c.publish("S", s)
     print("ok")
     #c.disconnect()
 
-m(t="status",m="connect success!")
+#m(t="status",m="connect success!")
